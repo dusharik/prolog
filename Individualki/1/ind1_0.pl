9ba425644a77b9d1e7ee72de8019bceb7b4ec7dc
dusharik <larik2001_01@mail.ru>
2021-03-12 21:21:02 +0300

    Create ind1_0.pl

---
+++ Individualki/1/ind1_0.pl
@@ -0,0 +1,54 @@
+man(maxim).
+man(kirill).
+man(sergey).
+man(ivan).
+man(nikolas).
+man(arseny).
+man(dmitry).
+man(jack).
+man(andrey).
+
+women(ksenia).
+women(daria).
+women(maria).
+women(jasmin).
+women(alina).
+women(polina).
+women(victoria).
+women(elisaveta).
+women(kristina).
+
+parent(maxim, kirill).
+parent(maxim, maria).
+parent(maxim, ivan).
+parent(maxim, alina).
+
+parent(ksenia, kirill).
+parent(ksenia, maria).
+parent(ksenia, ivan).
+parent(ksenia, alina).
+
+parent(kirill, arseny).
+parent(kirill, dmitry).
+parent(daria, arseny).
+parent(daria, dmitry).
+
+parent(sergey, jack).
+parent(sergey, polina).
+parent(daria, arseny).
+parent(daria, dmitry).
+
+parent(ivan, victoria).
+parent(ivan, elisaveta).
+parent(maria, victoria).
+parent(maria, elisaveta).
+
+parent(nikolas, kristina).
+parent(nikolas, andrey).
+parent(alina, kristina).
+parent(alina, andrey).
+
+
+
+
+
